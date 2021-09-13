ba11d0c3028ef71df0d0b1691d18e23d5b2fc2a0
shinaz <shinattey10@gmail.com>
2021-09-13 16:28:12 +0500

    finished project

--- djangotodo/todo/views.py
+++ djangotodo/todo/views.py
@@ -1,10 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
+from .forms import TodoForm
 from .models import Todo
 
 
 def todos(request):
-    todos = Todo.objects.all()
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
 
-    return render(request, 'todos.html', {'todos': todos})
+        if form.is_valid():
+            form.save()
+
+            return redirect('todos')
+    else:
+        form = TodoForm()
+
+    todos = Todo.objects.filter(is_done=False)
+
+    return render(request, 'todos.html', {'todos': todos, 'form': form})
+
+
+def todo(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+
+    change_status = request.GET.get('change_status', '')
+
+    if change_status:
+        todo.is_done = True
+        todo.save()
+
+    return render(request, 'todo.html', {'todo': todo} )
+
 
